72912a2a01bad91bba5d5243b71fe3c46c78202b
ulmaridae <lmaridae@icloud.com>
2020-10-28 15:14:55 +0300

    final solution

--- ArmstrongNumbers/src/Main.java
+++ ArmstrongNumbers/src/Main.java
@@ -1,39 +1,107 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
 
+    private static Map<Integer, Long> digitsPowers = new HashMap<>();
+    private static Set<Long> results = new TreeSet<>();
+
+    // recursive values
+    private static Deque<Integer> currentDigits = new LinkedList<>();
+    private static long currentValue;
+
+//    public static void main(String[] args) {
+//        System.out.println(Arrays.toString(getNumbers(1234)));
+//    }
+
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(getNumbers(1234)));
+        long a = System.currentTimeMillis();
+        System.out.println(Arrays.toString(getNumbers(10_000_000_000L)));
+        long b = System.currentTimeMillis();
+        System.out.println("memory " + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / (8 * 128 * 1024));
+        System.out.println("time = " + (b - a) / 1000);
     }
 
     public static long[] getNumbers(long N) {
-        long[] result = null;
-        List<Long> resultList = new ArrayList<>();
-        isArmstrong(N);
-        for (long i = 1; i < N; i++) {
-            if (isArmstrong(i)) {
-                resultList.add(i);
-            }
-        }
-        return resultList.stream().mapToLong(number -> number).toArray();
-    }
+        int digitAmount = String.valueOf(N).length();
 
-    private static boolean isArmstrong(long number) {
-        if (number <= 0) {
-            return false;
+        for (int i = 1; i <= digitAmount; i++) {
+            fillDigitsPowers(i);
+            currentDigits.clear();
+            currentValue = 0;
+            recursiveSearch(i);
         }
 
-        long[] digits = String.valueOf(number)
-                .chars()
-                .mapToLong(digit -> digit - '0')
+        return results.stream()
+                .mapToLong(value -> value)
+                .filter(value -> value < N)
                 .toArray();
-
-        long sum = Arrays.stream(digits)
-                .map(digit -> (long) Math.pow(digit, digits.length))
-                .sum();
-
-        return sum == number;
     }
+
+    private static void fillDigitsPowers(int digitCount) {
+        for (int digit = 0; digit < 10; digit++) {
+            digitsPowers.put(digit, (long) Math.pow(digit, digitCount));
+        }
+    }
+
+    private static void recursiveSearch(int digitAmount) {
+
+        int currentValueLength = String.valueOf(currentValue).length();
+        int lastDigit = currentDigits.peekLast() == null ? 0 : currentDigits.peekLast();
+
+        if (currentValueLength > digitAmount || currentDigits.size() == digitAmount) {
+            if (currentValueLength == digitAmount) {
+
+                Set<Integer> digitsInQueue = new HashSet<>(currentDigits);
+
+                Set<Integer> digitsInNumber = String.valueOf(currentValue)
+                        .chars()
+                        .map(value -> value - '0')
+                        .boxed()
+                        .collect(Collectors.toSet());
+
+                if (digitsInQueue.equals(digitsInNumber)) {
+                    results.add(currentValue);
+                }
+            }
+            return;
+        }
+
+        for (int i = lastDigit; i < 10; i++) {
+            currentDigits.add(i);
+            currentValue += digitsPowers.get(i);
+            recursiveSearch(digitAmount);
+            currentValue -= digitsPowers.get(i);
+            currentDigits.pollLast();
+        }
+    }
+
+
+//    public static long[] getNumbers(long N) {
+//        List<Long> resultList = new ArrayList<>();
+//        isArmstrong(N);
+//        for (long i = 1; i < N; i++) {
+//            if (isArmstrong(i)) {
+//                resultList.add(i);
+//            }
+//        }
+//        return resultList.stream().mapToLong(number -> number).toArray();
+//    }
+//
+//    private static boolean isArmstrong(long number) {
+//        if (number <= 0) {
+//            return false;
+//        }
+//
+//        long[] digits = String.valueOf(number)
+//                .chars()
+//                .mapToLong(digit -> digit - '0')
+//                .toArray();
+//
+//        long sum = Arrays.stream(digits)
+//                .map(digit -> (long) Math.pow(digit, digits.length))
+//                .sum();
+//
+//        return sum == number;
+//    }
 }
